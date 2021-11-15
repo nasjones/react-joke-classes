43cb33317331cbf198624e637ddb78273c647e27
nasjones <46979749+nasjones@users.noreply.github.com>
2021-11-14 23:08:23 -0500

    refactor to use classes instead of functions

---
+++ src/JokeList.js
@@ -0,0 +1,94 @@
+import React from "react";
+import axios from "axios";
+import Joke from "./Joke";
+import "./JokeList.css";
+
+class JokeList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { jokes: [] };
+		this.getJokes = this.getJokes.bind(this);
+		this.generateNewJokes = this.generateNewJokes.bind(this);
+		this.vote = this.vote.bind(this);
+	}
+
+	async getJokes() {
+		let j = [...this.state.jokes];
+		let seenJokes = new Set();
+		try {
+			while (j.length < this.props.numJokesToGet) {
+				let res = await axios.get("https://icanhazdadjoke.com", {
+					headers: { Accept: "application/json" },
+				});
+				let { status, ...jokeObj } = res.data;
+
+				if (!seenJokes.has(jokeObj.id)) {
+					seenJokes.add(jokeObj.id);
+					j.push({ ...jokeObj, votes: 0 });
+				} else {
+					console.error("duplicate found!");
+				}
+			}
+			this.setState({ jokes: j });
+		} catch (e) {
+			console.log(e);
+		}
+	}
+
+	async componentDidMount() {
+		if (this.state.jokes.length === 0) this.getJokes();
+	}
+
+	async componentDidUpdate() {
+		if (this.state.jokes.length === 0) this.getJokes();
+	}
+
+	/* empty joke list and then call getJokes */
+
+	generateNewJokes() {
+		this.setState({ jokes: [] });
+	}
+
+	/* change vote for this id by delta (+1 or -1) */
+
+	vote(id, delta) {
+		this.setState((prevState) => {
+			let newVotes = prevState.jokes.map((j) =>
+				j.id === id ? { ...j, votes: j.votes + delta } : j
+			);
+			return { jokes: newVotes };
+		});
+	}
+
+	render() {
+		if (this.state.jokes.length) {
+			let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes);
+
+			return (
+				<div className="JokeList">
+					<button className="JokeList-getmore" onClick={this.generateNewJokes}>
+						Get New Jokes
+					</button>
+
+					{sortedJokes.map((j) => (
+						<Joke
+							text={j.joke}
+							key={j.id}
+							id={j.id}
+							votes={j.votes}
+							vote={this.vote}
+						/>
+					))}
+				</div>
+			);
+		}
+
+		return null;
+	}
+}
+
+JokeList.defaultProps = {
+	numJokesToGet: 10,
+};
+
+export default JokeList;
